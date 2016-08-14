934ecffa1bd5f57c10df16706f846efe9635ecde
Daniel Mangowi <danielmangowi@gmail.com>
2016-08-14 16:26:14 +0200

    Removing generated routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'sections/index'
-
-  get 'sections/show'
-
-  get 'sections/new'
-
-  get 'sections/create'
-
-  get 'sections/edit'
-
-  get 'sections/update'
-
-  get 'sections/delete'
-
-  get 'sections/destroy'
-
-  get 'pages/index'
-
-  get 'pages/show'
-
-  get 'pages/new'
-
-  get 'pages/create'
-
-  get 'pages/edit'
-
-  get 'pages/update'
-
-  get 'pages/delete'
-
-  get 'pages/destroy'
-
   root  'demo#index'
   match ':controller(/:action(/:id))', :via => [:get, :post]
 
